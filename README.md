acde8ea6f1f992fd98703704d7db1bcff45818bf
Hakan <sanials4life2@gmail.com>
2022-02-17 19:49:25 +0100

    Env variables, Build & Run command, README and config file added :white_check_mark:

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# KnightOnline-MSSQL
+
+# Knight Online NodeJS - MSSQL
+
+This project allows you to make the CRUD operations on Knight Online database.
+
+#### Commands
+```
+npm run dev
+npm run build
+git run start
+```
+
+
+
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,11 @@
 import sql from "mssql";
+import config from "../config";
 
 const dbConfig = {
-  user: "hkn",
-  password: "qwerty123",
-  server: "x.dreamyko.com",
-  database: "KN_Online",
+  user: config.dbUser,
+  password: config.dbPassword,
+  server: config.dbServer,
+  database: config.dbDatabase,
   options: {
     encrypt: true,
     trustServerCertificate: true,
